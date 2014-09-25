d9a448e38c6f7189b2ecb7e697ca359e2f0940cf
Stephen LaPorte <stephen.laporte@gmail.com>
2014-09-24 19:44:59 -0700

    adding anchorific.js, but more styling needed

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,21 @@
+$(function() {
+    $('.content').anchorific({
+        navigation: '.anchorific', // position of navigation
+        speed: 200, // speed of sliding back to top
+        anchorClass: 'anchor', // class of anchor links
+        anchorText: ' #', // prepended or appended to anchor headings
+        top: '.top', // back to top button or link class
+        spy: true, // highlight active links as you scroll
+        position: 'append' // position of anchor text
+    });
 
+    $(window).scroll (function() {
+      var y = $(window).scrollTop();
+      // top offset of aside.anchorfic
+      if (y >= 325) {
+        $('.anchorific').addClass('fixed');
+      } else {
+        $('.anchorific').removeClass('fixed');
+      }
+    });
+})
